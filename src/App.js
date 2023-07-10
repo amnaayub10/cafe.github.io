e13ed1f833025a390427cc6b84024649f92ad728
amnaayub10 <138980787+amnaayub10@users.noreply.github.com>
2023-07-10 13:14:03 +0500

    cafe

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant'
+const app  = () => {
+  return (
+    <div>
+      <Resturant/>
+    </div>
+  )
+}
+
+export default app 
